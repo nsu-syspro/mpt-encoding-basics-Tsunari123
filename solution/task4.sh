5055bf585e5f296a2cbb4ac7c9aa00a91ced3c4f
NikRusDev <grumiktiktok@gmail.com>
2025-09-25 22:07:50 +0800

    Update task4.sh

--- solution/task4.sh
+++ solution/task4.sh
@@ -2,10 +2,4 @@
 
 # Вывести дату в формате:
 #   Freitag, 13. Oktober 2023
-export LC_TIME=en_EN.UTF-8
-
-date_input="$1"
-
-formatted_date=$(date -d "$date_input" '+%A, %d. %B %Y' 2>/dev/null)
-
-echo "$formatted_date"
+LC_ALL=de_DE.UTF-8 date '+%A, %d. %B %Y' -d "$@"
