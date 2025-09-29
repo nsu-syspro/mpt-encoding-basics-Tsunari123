a37b9e083544f30f064fd4cc5e9d4e48a4572d52
NikRusDev <grumiktiktok@gmail.com>
2025-09-29 23:53:19 +0700

    Update task2.sh

--- solution/task2.sh
+++ solution/task2.sh
@@ -2,8 +2,4 @@
 
 # Вывести время в формате:
 #   15 час/а/ов 23 минут/ы
-time_str="$1"
-hours=$(echo "$time_str" | cut -d: -f1)
-minutes=$(echo "$time_str" | cut -d: -f2)
-seconds=$(echo "$time_str" | cut -d: -f3)
-echo "$hours час/а/ов $minutes минут/ы"
+LC_TIME=ru_RU.UTF-8 date '+%H час/а/ов %M минут/ы' -d "$@"
